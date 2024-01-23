b020c3ddefb904ba062aefd3789e7165d5d00b67
Bayelaye313 <bayelaye0510@gmail.com>
2024-01-23 09:36:47 +0000

    test

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,20 +1,20 @@
 #include "sort.h"
 
 /**
- * selection_sort - A function that uses the Selection Sort algorithm.
+ * selection_sort - A function that use selection sort algorithm.
  * @array: An array to sort.
  * @size: The size of the array.
  * Return: Nothing.
  */
 void selection_sort(int *array, size_t size)
 {
+	int aux = 0;
+	size_t i = 0, j = 0, pos = 0;
+
 	if (array == NULL || size == 0)
 		return;
 
-	size_t i, j, pos;
-	int tmp;
-
-	for (i = 0; i < size - 1; i++)
+	for (; i < size - 1; i++)
 	{
 		pos = i;
 		for (j = i + 1; j < size; j++)
@@ -22,12 +22,11 @@ void selection_sort(int *array, size_t size)
 			if (array[j] < array[pos])
 				pos = j;
 		}
-
 		if (pos != i)
 		{
-			tmp = array[i];
+			aux = array[i];
 			array[i] = array[pos];
-			array[pos] = tmp;
+			array[pos] = aux;
 			print_array(array, size);
 		}
 	}
